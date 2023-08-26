c44c5eeab793d7f65862eff9eeceaaed99b8ed9b
Mohammed Ruknuddin <theruknologydeb@dell-pc.localdomain>
2023-08-26 23:35:11 +0300

    day1

--- aoc1.c
+++ aoc1.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main () {
-  printf("okay then");
-  return 0;
+    printf("okay then");
+    return 0;
+}
+
+int maxChecker (int arr[], int n) {
+    for (i = 0; i < n; i++) {
+        printf("%d \n", arr[i]);
+    }
+
+    printf("\n");
 }
 
