80627de1931be5540e759a92823a0dc5497509ea
Mohammed Ruknuddin <theruknologydeb@dell-pc.localdomain>
2023-08-28 00:37:44 +0300

    Memory Allocation [Day 1]

--- aoc1.c
+++ aoc1.c
@@ -1,24 +1,61 @@
-#include<stdio.h>
-int main () {
-    printf("okay then");
+#include <stdio.h>
+#include <stdlib.h>
+
+int *listCounter(int arr[], int n, int *resultLength);
+
+int main()
+{
+    int testList[] = {55, 45, 0, 1000, 200};
+    int testLength = sizeof(testList) / sizeof(testList[0]);
+
+    int resultLength;
+    int *resultList = listCounter(testList, testLength, &resultLength);
+
+    for (int i = 0; i < resultLength; i++)
+    {
+        printf("%d\n", resultList[i]);
+    }
+
+    free(resultList);
     return 0;
 }
 
-int listCounter (int arr[], int n) {
-
+int *listCounter(int arr[], int n, int *resultLength)
+{
+    int *counterList = NULL;
     int counter = 0;
-    int counterList[n] = {}; 
     int currentI = 0;
-    for (i = 0; i < n; i++) {
-        if (arr[i] != "") {
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
             counter += arr[i];
-        } else {
-            counterList[currentI] = counter; 
+        }
+        else
+        {
+            counterList = (int *)realloc(counterList, (currentI + 1) * sizeof(int));
+            if (counterList == NULL)
+            {
+                // Handle memory allocation error
+                return NULL;
+            }
+
+            counterList[currentI] = counter;
             currentI += 1;
             counter = 0;
         }
     }
 
-    printf("\n");
-}
+    counterList = (int *)realloc(counterList, (currentI + 1) * sizeof(int));
+    if (counterList == NULL)
+    {
+        // Handle memory allocation error
+        return NULL;
+    }
 
+    counterList[currentI] = counter;
+    *resultLength = currentI + 1;
+
+    return counterList;
+}
